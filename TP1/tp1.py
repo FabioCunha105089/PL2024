4075a66b701208a2ff8102e523bfdf6ee0f0580f
Fábio Cunha <a105089@alunos.uminho.pt>
2024-02-09 18:05:19 +0000

    TP1 final

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -1,35 +1,40 @@
 import sys
 
-first = True
 ages = {}
 genres = set()
-fit = 0
+fit_count = 0
 total = 0
-for i, line in enumerate(sys.stdin):
-    if first:
-        continue
 
-    _, _, _, _, _, age, _, _, genre, _, _, _, fit = line.split(',')
-    if fit:
-        fit += 1
+next(sys.stdin)
+
+for i, line in enumerate(sys.stdin):
+    _, _, _, _, _, age_str, _, _, genre, _, _, _, fit = line.split(',')
+    age = int(age_str)
+    if 't' in fit:
+        fit_count += 1
 
     genres.add(genre)
 
-    r = age % 10
-    if r < 5:
-        ages.update({f"[{age - r}, {age + 4 - r}]": age})
+    lower_bound = (age // 5) * 5
+    interval = f"[{lower_bound}, {lower_bound + 4}]"
+
+    if interval in ages:
+        ages[interval] += 1
     else:
-        ages.update({f"[{age - r + 5}, {age + 9 - r}]": age})
+        ages[interval] = 1
 
     total += 1
 
 genres_list = sorted(genres)
-print(', '.join(genres_list))
+print(f"Modalidades: {', '.join(genres_list)}")
 
-fit_athletes = fit / total * 100
+fit_athletes = fit_count / total * 100
 unfit_athletes = 100 - fit_athletes
-print(f"""Fit -> {fit_athletes}%
-Unfit -> {unfit_athletes}%""")
+print(f"""Aptos: {fit_athletes}%
+Inaptos: {unfit_athletes}%""")
 
-for interval, athlete_ages in ages.items():
-    print(f"{interval} -> {len(athlete_ages/total*100)}%")
+print("Distribuição: ")
+sorted_intervals = sorted(ages.keys())
+for interval in sorted_intervals:
+    percentage = ages[interval] / total * 100
+    print(f"{interval}: {percentage:.2f}%")
