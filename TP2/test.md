1599955b4cdac949a689e3c81eea869e9db6a59a
Fabio <a105089@alunos.uminho.pt>
2024-02-25 00:40:25 +0000

    tp3 estava na pasta tp2. tp2 adicionado

---
+++ TP2/test.md
@@ -0,0 +1,25 @@
+# Markdown to HTML Conversion Test
+
+This is a **bold text** example. 
+
+This is *italic text* example.
+
+## Header 2
+
+### Header 3
+
+#### Header 4
+
+- This is a list item 1
+- This is a list item 2
+- This is a list item 3
+
+1. This is an ordered list item 1
+2. This is an ordered list item 2
+3. This is an ordered list item 3
+
+Here is a [link to Google](https://www.google.com/).
+
+Here is an image:
+
+![Alt text for the image](https://via.placeholder.com/150)
--- TP2/tp2.py
+++ TP2/tp2.py
@@ -1,21 +1,72 @@
-import re
 import sys
-pattern = re.compile(r'on|off|=|[+\-]?\d+', re.IGNORECASE)
+import re
 
+html = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Traducao MD</title>
+</head>
+<body>
+"""
 
-def find_items(l):
-    return [m.group() for m in pattern.finditer(l)]
+in_oli = False
+in_uli = False
 
-
-total = 0
-active = True
 for line in sys.stdin:
-    for item in find_items(line):
-        if item == 'on':
-            active = True
-        elif item == 'off':
-            active = False
-        elif item == '=':
-            print(total)
-        elif active:
-            total += int(item)
+    make_p = True
+
+    line = line.strip()
+
+    pattern = re.compile(r'^#+')
+    match = pattern.match(line)
+    if match:
+        make_p = False
+        count = len(match.group(0))
+        line = f"<h{count}>{line[count+1:]}</h{count}>\n"
+
+    line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)
+    line = re.sub(r'\*(.*?)\*', r'<i>\1</i>', line)
+
+    match = re.match(r'^(\d+\.|-)', line)
+    if match:
+        make_p = False
+        if match.group(1).endswith('.'):
+            if not in_oli:
+                if in_uli:
+                    html += "</ul>\n"
+                    in_uli = False
+                html += "<ol>\n"
+                in_oli = True
+        elif match.group(1) == '-':
+            if not in_uli:
+                if in_oli:
+                    html += "</ol>\n"
+                    in_oli = False
+                html += "<ul>\n"
+                in_uli = True
+        line = f"<li>{line[3 if match.group(1).endswith('.') else 2:]}</li>\n"
+    elif not match:
+        if in_oli:
+            html += "</ol>\n"
+            in_oli = False
+        elif in_uli:
+            html += "</ul>\n"
+            in_uli = False
+
+    line = re.sub(r'!\[(.*?)\]\((.*?)\)', r'<img src="\2" alt="\1"/>\n', line)
+    line = re.sub(r'\[(.*?)\]\((.*?)\)', r'<a href="\2">\1</a>', line)
+
+    if make_p and line:
+        line = "<p>" + line + "</p>\n"
+
+    html += line
+
+html += """<footer>
+<p><i>Ficheiro de teste gerado, a pedido, pelo ChatGPT</i></p>
+</footer>
+</body>
+</html>\n"""
+
+with open('traducao.html', 'w') as file:
+    file.write(html)
